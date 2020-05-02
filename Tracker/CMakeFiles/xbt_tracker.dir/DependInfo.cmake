8a4427c912ce97d4a8b23e86daab7ce6672b688b
iseeyoucopy <iseeyoucopy@yahoo.com>
2020-05-02 11:10:32 +0100

    First commit

---
+++ Tracker/CMakeFiles/xbt_tracker.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/var/www/xbt/Tracker/config.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/config.cpp.o"
+  "/var/www/xbt/Tracker/connection.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/connection.cpp.o"
+  "/var/www/xbt/Tracker/epoll.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/epoll.cpp.o"
+  "/var/www/xbt/Tracker/tracker.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/tracker.cpp.o"
+  "/var/www/xbt/Tracker/tracker_input.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/tracker_input.cpp.o"
+  "/var/www/xbt/Tracker/transaction.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/transaction.cpp.o"
+  "/var/www/xbt/misc/bt_misc.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/bt_misc.cpp.o"
+  "/var/www/xbt/misc/database.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/database.cpp.o"
+  "/var/www/xbt/misc/sha1.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/sha1.cpp.o"
+  "/var/www/xbt/misc/socket.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/socket.cpp.o"
+  "/var/www/xbt/misc/sql_query.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/sql_query.cpp.o"
+  "/var/www/xbt/misc/xcc_z.cpp" "/var/www/xbt/Tracker/CMakeFiles/xbt_tracker.dir/var/www/xbt/misc/xcc_z.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EPOLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../misc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
